e590e897c1a758e789f7ed12e6ff2db30a518670
Asher Friedman <asher.d.friedman@gmail.com>
2019-06-06 15:51:31 -0400

    after cleaning up and recopying original files

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
+
+from learning_logs.models import Topic, Entry
 
 from learning_logs.models import Topic
 
 admin.site.register(Topic)
-
-# Register your models here.
+admin.site.register(Entry)
